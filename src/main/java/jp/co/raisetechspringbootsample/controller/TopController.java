4453da16719054774269964b99c96bf137c74ad3
yo-chan <whitesnow.wave@gmail.com>
2022-04-27 18:12:24 +0900

    first try for Tymeleaf

--- src/main/java/jp/co/raisetechspringbootsample/controller/TopController.java
+++ src/main/java/jp/co/raisetechspringbootsample/controller/TopController.java
@@ -1,6 +1,7 @@
 package jp.co.raisetechspringbootsample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,9 @@ public class TopController {
 //	todo: add service by DI
 
 	@GetMapping
-	public String index() {
+	public String index(Model m) { // Modelを使うとTymeleafのhtml側に値を渡す
+		String messageString = "wasshoi";
+		m.addAttribute("message", messageString); // "message"という名のmessageStringを渡す
 		return "index";
 
 	}
