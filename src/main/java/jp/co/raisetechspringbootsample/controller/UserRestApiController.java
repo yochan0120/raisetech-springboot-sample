c6c7d1a40da0a1daec403bfa0a67f62348db0b85
yo-chan <whitesnow.wave@gmail.com>
2022-05-06 11:56:00 +0900

    add UserService,UserRepository

--- src/main/java/jp/co/raisetechspringbootsample/controller/UserRestApiController.java
+++ src/main/java/jp/co/raisetechspringbootsample/controller/UserRestApiController.java
@@ -3,33 +3,26 @@ package jp.co.raisetechspringbootsample.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.raisetechspringbootsample.domain.User;
 import jp.co.raisetechspringbootsample.service.UserService;
 
-//import jp.co.raisetechspringbootsample.service.UserService;
-
-@Controller
-public class TopController {
-
+@RestController
+public class UserRestApiController {
 //	todo: add service by DI
 	@Autowired
 	UserService userService;
 
-	@GetMapping
-	public String index(Model m) { // Modelを使うとTymeleafのhtml側に値を渡す
+	@GetMapping("/api")
+	public List<User> getUsers() { // Modelを使うとTymeleafのhtml側に値を渡す
 
-		m.addAttribute("message", "wasshoi");
-
-		// ユーザーDB情報を取得
+		// ユーザー情報取得
 		List<User> userList = userService.getUsers();
-		System.out.println(userList);
-		m.addAttribute(userList); // "message"という名のmessageStringを渡す
+		// m.addAttribute(userList); // "message"という名のmessageStringを渡す
 //		m.addAttribute("userList", "userList"); 　//同じ意味 usetListに名前を付けるならこっち
 
-		return "index";
+		return userList;
 	}
 }
